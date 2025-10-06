95b5b6f85c89bb561174bce0f53f63c8c990be6d
krizopras <krizopras14@gmail.com>
2025-10-06 11:38:22 +0300

    Update and rename main.py to main_railway.py

--- main_railway.py
+++ main_railway.py
@@ -157,4 +157,4 @@ async def get_team_stats(team_name: str):
 if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, log_level="info")
+    uvicorn.run("main_railway:app", host="0.0.0.0", port=port, log_level="info")
